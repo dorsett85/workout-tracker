31d48d23801dc41f329491f8021d174b7c71c870
Clayton Phillips-Dorsett <claytonphillipsdorsett@gmail.com>
2019-02-21 22:39:47 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    /* Proxy to a backend, add appropriate url and port
+    proxy: {
+      '**': 'http://127.0.0.1:8000/'
+    }
+    */
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        include: path.resolve(__dirname, 'src'),
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: "css-loader",
+          options: {
+            modules: true,
+            localIdentName: '[name]_[hash]',
+          }
+        }, {
+          loader: "sass-loader"
+        }]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]'
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src/index.html')
+    }),
+  ]
+};
