c5355baa183393345a8b47baaac3411351d7076f
clayton <claytonphillipsdorsett@gmail.com>
2019-05-14 22:12:25 -0400

    Fixed workout chart hover bug

--- frontend/src/components/User/Workout/WorkoutEditor/WorkoutEditorChart.js
+++ frontend/src/components/User/Workout/WorkoutEditor/WorkoutEditorChart.js
@@ -41,20 +41,24 @@ const getWorkoutSeries = (workoutData) => {
   return chartData;
 };
 
-// onMousOver javascript for the chart title (plain text to work with highcharts)
+// onMousEenter function for the chart title (plain text to work with highcharts)
 const onMouseOverInfo = `
-  const infoEl = document.getElementById('chartTitleInfo');
-  const chartInfoAlert = document.createElement('div');
-  chartInfoAlert.id = 'chartInfoAlert';
-  chartInfoAlert.classList.add('alert', 'alert-warning', 'p-2', 'text-center');
-  chartInfoAlert.style.boxShadow = '2px 2px 6px 0px grey';
-  chartInfoAlert.innerHTML = '<small>Use numbers in the table<br>for better chart progress</small>';
-  infoEl.insertAdjacentElement('afterEnd', chartInfoAlert);
+  (() => {
+    const infoEl = document.getElementById('chartTitleInfo');
+    const chartInfoAlert = document.createElement('div');
+    chartInfoAlert.id = 'chartInfoAlert';
+    chartInfoAlert.classList.add('alert', 'alert-warning', 'p-2', 'text-center');
+    chartInfoAlert.style.boxShadow = '2px 2px 6px 0px grey';
+    chartInfoAlert.innerHTML = '<small>Use numbers in the table<br>for better chart progress</small>';
+    infoEl.insertAdjacentElement('afterEnd', chartInfoAlert);
+  })()
 `;
 
-// onMouseLeave javascript for the chart title (plain text to work with highcharts)
+// Mouseenter function for the chart title (plain text to work with highcharts)
 const onMouseLeaveInfo = `
-  document.getElementById('chartInfoAlert').remove();
+  (() => {
+    document.getElementById('chartInfoAlert').remove();
+  })()
 `;
 
 const createChart = (workoutData) => {
